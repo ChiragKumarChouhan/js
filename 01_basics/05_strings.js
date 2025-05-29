9711d5fefb3ef1ad8768185c15d4b5959db4e65d
ChiragKumarChouhan <chiragneeraj2@gmail.com>
2025-05-29 14:13:35 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,10 @@
+const name = "chirag"
+const repoCount = 50
+
+
+//console.log(name +  repoCount + "value"); not a good practice 
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`); // good practice 
+
+
+vf
